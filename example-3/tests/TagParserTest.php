03f0c8ec93575c2e106b1ac957e085b2c3058bf7
Jeffrey Way <jeffrey@jeffrey-way.com>
2020-12-16 09:50:07 -0500

    Episode 4 complete

---
+++ example-3/tests/TagParserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests;
+
+use App\TagParser;
+use PHPUnit\Framework\TestCase;
+
+class TagParserTest extends TestCase
+{
+    /**
+     * @dataProvider tagsProvider
+     */
+    public function test_it_parses_tags($input, $expected)
+    {
+        $parser = new TagParser();
+
+        $result = $parser->parse($input);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function tagsProvider()
+    {
+        return [
+            ["personal", ["personal"]],
+            ["personal, money, family", ["personal", "money", "family"]],
+            ["personal, money, family", ["personal", "money", "family"]],
+            ["personal | money | family", ["personal", "money", "family"]],
+            ["personal|money|family", ["personal", "money", "family"]],
+            ["personal!money!family", ["personal", "money", "family"]],
+        ];
+    }
+}
